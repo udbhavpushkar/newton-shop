c8e33ffdb0061f8206f3268b26254c834c518ec4
udbhavpushkar <udbhavpushkar@gmail.com>
2023-04-26 22:47:07 +0530

    added timestamps in model

--- models/product.js
+++ models/product.js
@@ -6,7 +6,10 @@ const productSchema = mongoose.Schema({
     price: { type: Number },
     category: { type: String, required: true },
     details: { type: String }
-})
+}, {
+    timestamps: true
+}
+)
 
 const Product = mongoose.model("Product", productSchema)
 
